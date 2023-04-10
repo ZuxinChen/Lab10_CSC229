d81b0a193de896383f53270e10540bc9d7177d85
ZuxinChen <116303234+ZuxinChen@users.noreply.github.com>
2023-04-10 16:08:16 -0400

    finish

---
+++ Lab10_CSC229/src/main/java/com/mycompany/lab10_csc229/Lab10_CSC229.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab10_csc229;
+
+/**
+ *
+ * @author user
+ */
+public class Lab10_CSC229 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
